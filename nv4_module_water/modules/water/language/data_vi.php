8eba68cdf2e4e67941fb84b256cfb0db92b8185d
Nguyễn Thanh Hoàng <hoang.nguyen@webvang.vn>
2015-09-09 16:38:52 +0700

    import data demo

---
+++ nv4_module_water/modules/water/language/data_vi.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Webvang (hoang.nguyen@webvang.vn)
+ * @Copyright (C) 2015 Webvang. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate 2-10-2010 20:59
+ */
+
+if( ! defined( 'NV_ADMIN' ) ) die( 'Stop!!!' );
+
+/**
+ * Note:
+ * 	- Module var is: $lang, $module_file, $module_data, $module_upload, $module_theme, $module_name
+ * 	- Accept global var: $db, $db_config, $global_config
+ */
